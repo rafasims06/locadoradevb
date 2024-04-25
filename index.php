b6896c134655bafe12db24cc70b46566107f2708
Rafaela Sims <rafa.sims456@gmail.com>
2024-04-25 11:54:39 -0300

    Primeira aula finalizada

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=d evice-width, initial-scale=1.0">
+            
+    <title>LOCADORA DEV</title>
+    <link rel="stylesheet" href="./css/estilo.css">
+</head>
+<body>
+    <header>
+       <h1>LOCADORA DEV</H1>
+      <ul>
+        <li>
+           <a href="index.php?menu=home">Home</a>
+        </li>
+        <li>
+            <a href="index.php?menu=lista-videos">Vídeos</a>
+        </li>
+        <li>
+            <a href="index.php?menu=lista-categorias">Categorias</a>
+        </li>
+        <li>
+            <a href="index.php?menu=lista-clientes">Clientes</a>
+        </li>
+        <li>
+            <a href="index.php?menu=lista-locacoes">Locações</a>
+        </li>
+
+        </li>
+     </ul>
+    </header>
+</body>
+</html>
